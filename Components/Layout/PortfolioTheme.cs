6207ffb2a46ba1f467aa53fb2bd093f64f0a09ee
Annika <annikazarske49@gmail.com>
2024-02-15 14:25:39 +0100

    Add project files.

---
+++ Components/Layout/PortfolioTheme.cs
@@ -0,0 +1,30 @@
+﻿using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Components.Layout
+{
+    public class PortfolioTheme
+    {
+        public static MudTheme theme = new MudTheme()
+        {
+            Palette = new PaletteLight()
+            {
+                Primary = "#F5DD89"
+            },
+            PaletteDark = new PaletteDark()
+            {
+                //Primary = Colors.Blue.Lighten1
+            },
+
+            LayoutProperties = new LayoutProperties()
+            {
+                //DrawerWidthLeft = "260px",
+                //DrawerWidthRight = "300px"
+            }
+        };
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
+
+namespace Portfolio
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("Estedad-Regular.ttf", "EstedadRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            //builder.Services.AddMudPopoverService();
+            builder.Services.AddMudServices();
+
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
